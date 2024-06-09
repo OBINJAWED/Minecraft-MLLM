d23d3410fcc85f51e0124ed59b97d38baf244ede
OBINJAWED <Omer.BinJawed@gmail.com>
2024-06-09 12:59:14 -0400

    Initial commit

---
+++ src/main/java/name/modid/Send_and_receive_msgsDataGenerator.java
@@ -0,0 +1,11 @@
+package name.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Send_and_receive_msgsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/name/modid/Send_and_receive_msgs_Client.java
@@ -0,0 +1,10 @@
+package name.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Send_and_receive_msgs_Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
